25e607cf0d802d2809b7e2d43e500fa6706ddb2c
Willian Torres da Cruz <50975425+uillia@users.noreply.github.com>
2021-09-01 01:48:22 -0300

    Add files via upload

---
+++ MySQL/Structure/suppliercompany.sql
@@ -0,0 +1,56 @@
+-- MySQL dump 10.13  Distrib 8.0.26, for Win64 (x86_64)
+--
+-- Host: localhost    Database: market
+-- ------------------------------------------------------
+-- Server version	8.0.26
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `suppliercompany`
+--
+
+DROP TABLE IF EXISTS `suppliercompany`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `suppliercompany` (
+  `idSupplierCompany` int NOT NULL AUTO_INCREMENT,
+  `idhierarchicalType` int DEFAULT NULL,
+  `nameCompany` varchar(45) DEFAULT NULL,
+  `cnpj` varchar(18) DEFAULT NULL,
+  `mainActivity` varchar(45) DEFAULT NULL,
+  `phone` varchar(16) DEFAULT NULL,
+  `adress` varchar(75) DEFAULT NULL,
+  `district` varchar(45) DEFAULT NULL,
+  `city` varchar(45) DEFAULT NULL,
+  `state` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `site` varchar(45) DEFAULT NULL,
+  `regDate` date DEFAULT NULL,
+  PRIMARY KEY (`idSupplierCompany`),
+  UNIQUE KEY `idSupplierCompany` (`idSupplierCompany`),
+  UNIQUE KEY `cnpj` (`cnpj`),
+  KEY `idhierarchicalType` (`idhierarchicalType`),
+  CONSTRAINT `suppliercompany_ibfk_1` FOREIGN KEY (`idhierarchicalType`) REFERENCES `hierarchicaltype` (`idhierarchicalType`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-09-01  0:56:43
